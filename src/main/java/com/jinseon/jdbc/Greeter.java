4f6384a4f8081dd7718b043cd76cb6b197011dba
jinseon <bongjs44@github.com>
2019-03-22 18:12:30 +0900

    add src

---
+++ src/main/java/com/jinseon/jdbc/Greeter.java
@@ -0,0 +1,11 @@
+package com.jinseon.jdbc;
+
+public class Greeter {
+	public Greeter() {
+		System.out.println("Greeter instance created.");
+	}
+	
+	public String greet() {
+		return "Hello, World!";
+	}
+}
---
+++ src/main/java/com/jinseon/jdbc/NormalMain.java
@@ -0,0 +1,14 @@
+package com.jinseon.jdbc;
+
+public class NormalMain {
+
+	public static void main(String[] args) {
+		Greeter g1 = new Greeter();
+		System.out.println(g1.greet());
+
+		Greeter g2 = new Greeter();
+		System.out.println(g2.greet());
+
+	}
+
+}
